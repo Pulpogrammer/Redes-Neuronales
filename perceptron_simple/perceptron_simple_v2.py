a19ece3627e632cbdaf77ab9fe933948cacbbb71
Santiago Vega <81502562+Pulpogrammer@users.noreply.github.com>
2026-04-20 02:42:57 -0300

    Update perceptron_simple_v2.py

--- perceptron_simple/perceptron_simple_v2.py
+++ perceptron_simple/perceptron_simple_v2.py
@@ -37,17 +37,16 @@ class PerceptronSimple:
             w[i]+= deltas_w[i]
         return w    
         
-    # calculamos el error promedio para cada ejemplos   
+    # calculo la cantidad de errores con el w actualizado  
     def calc_error_global(self, datos_entrada, w):
-        suma_errores = 0.0
+        suma_errores = 0
         for i in range(len(datos_entrada)):
             d_actual = datos_entrada[i]
             y_actual = self._y_deseadas[i]
             O = self.calcular_salida(d_actual, w)
-            
-            e = (y_actual - O)**2
-            suma_errores += e
-        return suma_errores / 2
+            if y_actual != O:
+                suma_errores += 1
+        return suma_errores
                 
     # tanto la exitacion y la salida se utilizan en el calculo principal del dato random como en el error global
     # para no repetir codigo se utiliza esta funcion que devuelve la salida
